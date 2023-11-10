fb2dab6db62b9a79507d49ffd854865bfdddeb05
chelunike <chelunike@gmail.com>
2023-11-10 13:08:14 +0100

    Añadida entidad coche

---
+++ sh_car_analizer/car.py
@@ -0,0 +1,22 @@
+
+
+class Model(object):
+	"""Representa el Modelo de un coche"""
+	_marca: str = ''
+	_modelo: str = ''
+	_combustible: str = ''
+	_año: int = 1900
+	_cv: int = 0
+	_kms: int = 0
+	_puertas: int = 0
+	_cambio_marchas: str = ''
+	_color: str = ''
+	_precio: float = 0.0
+	
+	def __init__(self, arg):
+		super(Model, self).__init__()
+		self.arg = arg
+
+
+
+
